27830cd05fd566cee9862811cc4efbcf0bef32ef
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2022-01-22 18:23:19 +0000

    [banderwagon/CRS]
    - Redo test vectors to match new CRS and the fact that we use banderwagon

--- src/crs.rs
+++ src/crs.rs
@@ -53,10 +53,9 @@ fn generate_random_elements(num_required_points: usize, seed: &'static [u8]) -> 
     use bandersnatch::Fq;
     use sha2::{Digest, Sha256};
 
-    let mut hasher = Sha256::new();
     let choose_largest = false;
 
-    (0u32..)
+    (0u64..)
         .into_iter()
         // Hash the seed + i to get a possible x value
         .map(|i| {
@@ -66,76 +65,60 @@ fn generate_random_elements(num_required_points: usize, seed: &'static [u8]) -> 
             let bytes: Vec<u8> = hasher.finalize().to_vec();
             bytes
         })
-        // The from_bytes method does not reduce the bytes, it expects the
+        // The Element::from_bytes method does not reduce the bytes, it expects the
         // input to be in a canonical format, so we must do the reduction ourselves
-        .map(|hash_bytes| bandersnatch::Fq::from_be_bytes_mod_order(&hash_bytes))
-        // Using the x co-ordinate fetch a possible y co-ordinate
-        .map(|x_coord| EdwardsAffine::get_point_from_x(x_coord, choose_largest))
-        .filter_map(|point| point)
-        // Double the point incase its not in the prime order subgroup
-        .map(|point| point.double())
-        // Serialise x co-ordinate of point
-        .map(|point| {
+        .map(|hash_bytes| Fq::from_be_bytes_mod_order(&hash_bytes))
+        .map(|x_coord| {
             let mut bytes = [0u8; 32];
-            point.x.serialize(&mut bytes[..]).unwrap();
+            x_coord.serialize(&mut bytes[..]).unwrap();
             // TODO: this reverse is hacky, and its because there is no way to specify the endianness in arkworks
             // TODO So we reverse it here, to be interopable with the banderwagon specs which needs big endian bytes
             bytes.reverse();
             bytes
         })
-        // Using banderwagon deserialise the x-cordinate to get a valid banderwagon element
+        // Deserialise the x-cordinate to get a valid banderwagon element
         .map(|bytes| Element::from_bytes(&bytes))
         .filter_map(|point| point)
         .take(num_required_points)
         .collect()
 }
 
-// TODO: update hackmd as we are now using banderwagon
-// TODO then redo this test
-// #[test]
-// fn crs_consistency() {
-//     // See: https://hackmd.io/1RcGSMQgT4uREaq1CCx_cg#Methodology
-//     use ark_serialize::CanonicalSerialize;
-//     use bandersnatch::Fq;
-//     use sha2::{Digest, Sha256};
+#[test]
+fn crs_consistency() {
+    // TODO: update hackmd as we are now using banderwagon and the point finding strategy
+    // TODO is a bit different
+    // See: https://hackmd.io/1RcGSMQgT4uREaq1CCx_cg#Methodology
+    use ark_serialize::CanonicalSerialize;
+    use bandersnatch::Fq;
+    use sha2::{Digest, Sha256};
 
-//     let points = generate_random_elements(256, b"eth_verkle_oct_2021");
-//     for point in &points {
-//         let on_curve = point.is_on_curve();
-//         let in_correct_subgroup = point.is_in_correct_subgroup_assuming_on_curve();
-//         if !on_curve {
-//             panic!("generated a point which is not on the curve")
-//         }
-//         if !in_correct_subgroup {
-//             panic!("generated a point which is not in the prime subgroup")
-//         }
-//     }
+    let points = generate_random_elements(256, b"eth_verkle_oct_2021");
 
-//     let mut bytes = [0u8; 32];
-//     points[0].serialize(&mut bytes[..]).unwrap();
-//     assert_eq!(
-//         hex::encode(&bytes),
-//         "22ac968a98ab6c50379fc8b039abc8fd9aca259f4746a05bfbdf12c86463c208",
-//         "the first point is incorrect"
-//     );
-//     let mut bytes = [0u8; 32];
-//     points[255].serialize(&mut bytes[..]).unwrap();
-//     assert_eq!(
-//         hex::encode(&bytes),
-//         "c8b4968a98ab6c50379fc8b039abc8fd9aca259f4746a05bfbdf12c86463c208",
-//         "the 256th (last) point is incorrect"
-//     );
+    let mut bytes = [0u8; 32];
+    points[0].serialize(&mut bytes[..]).unwrap();
+    assert_eq!(
+        hex::encode(&bytes),
+        "01587ad1336675eb912550ec2a28eb8923b824b490dd2ba82e48f14590a298a0",
+        "the first point is incorrect"
+    );
+    let mut bytes = [0u8; 32];
+    points[255].serialize(&mut bytes[..]).unwrap();
+    assert_eq!(
+        hex::encode(&bytes),
+        "3de2be346b539395b0c0de56a5ccca54a317f1b5c80107b0802af9a62276a4d8",
+        "the 256th (last) point is incorrect"
+    );
 
-//     let mut hasher = Sha256::new();
-//     for point in &points {
-//         let mut bytes = [0u8; 32];
-//         point.serialize(&mut bytes[..]).unwrap();
-//         hasher.update(&bytes);
-//     }
-//     let bytes = hasher.finalize().to_vec();
-//     assert_eq!(
-//         hex::encode(&bytes),
-//         "c390cbb4bc42019685d5a01b2fb8a536d4332ea4e128934d0ae7644163089e76",
-//         "unexpected point encountered"
-//     );
-// }
+    let mut hasher = Sha256::new();
+    for point in &points {
+        let mut bytes = [0u8; 32];
+        point.serialize(&mut bytes[..]).unwrap();
+        hasher.update(&bytes);
+    }
+    let bytes = hasher.finalize().to_vec();
+    assert_eq!(
+        hex::encode(&bytes),
+        "1fcaea10bf24f750200e06fa473c76ff0468007291fa548e2d99f09ba9256fdb",
+        "unexpected point encountered"
+    );
+}
