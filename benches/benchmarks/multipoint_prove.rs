09b51cf71699de3ea789d76333bc49024597add0
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2022-01-22 14:22:09 +0000

    modify benchmarks to match new API

--- benches/benchmarks/multipoint_prove.rs
+++ benches/benchmarks/multipoint_prove.rs
@@ -4,13 +4,13 @@ use ark_std::rand::SeedableRng;
 use ark_std::UniformRand;
 use bandersnatch::{EdwardsProjective, Fr};
 use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion};
-use ipa_bandersnatch::ipa::create;
-use ipa_bandersnatch::lagrange_basis::*;
-use ipa_bandersnatch::math_utils::inner_product;
-use ipa_bandersnatch::multiproof::*;
-use ipa_bandersnatch::slow_vartime_multiscalar_mul;
-use ipa_bandersnatch::transcript::TranscriptProtocol;
-use merlin::Transcript;
+use ipa_multipoint::ipa::create;
+use ipa_multipoint::lagrange_basis::*;
+use ipa_multipoint::math_utils::inner_product;
+use ipa_multipoint::multiproof::*;
+use ipa_multipoint::slow_vartime_multiscalar_mul;
+use ipa_multipoint::transcript::TranscriptProtocol;
+
 use rand_chacha::ChaCha20Rng;
 use std::iter;
 
@@ -24,7 +24,7 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     // Setup parameters, n is the degree + 1
     // CRs is the G_Vec, H_Vec, Q group elements
     let n = 256;
-    let crs = CRS::new(n);
+    let crs = CRS::new(n, b"random seed");
 
     let mut rng = test_rng();
     let poly = LagrangeBasis::new((0..n).map(|_| Fr::rand(&mut rng)).collect());
