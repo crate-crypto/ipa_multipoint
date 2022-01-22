08a4aa5233123ab3d3ce31f9ccf301b2f72ec417
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2022-01-22 17:09:49 +0000

    modify ipa and multiproof to use banderwagon

--- src/multiproof.rs
+++ src/multiproof.rs
@@ -1,13 +1,11 @@
 // We get given multiple polynomials evaluated at different points
 #![allow(non_snake_case)]
 
-use std::collections::HashMap;
-
-use crate::ipa::{self, IPAProof};
+use crate::crs::CRS;
+use crate::ipa::{self, slow_vartime_multiscalar_mul, IPAProof};
 use crate::lagrange_basis::{LagrangeBasis, PrecomputedWeights};
 use crate::math_utils::inner_product;
 use crate::math_utils::powers_of;
-use crate::slow_vartime_multiscalar_mul;
 use crate::transcript::Transcript;
 use crate::transcript::TranscriptProtocol;
 use ark_ec::{AffineCurve, ProjectiveCurve};
@@ -16,67 +14,14 @@ use ark_ff::{batch_inversion, Field};
 use ark_ff::{One, Zero};
 use ark_poly::univariate::DensePolynomial;
 use ark_poly::{Polynomial, UVPolynomial};
-use bandersnatch::multi_scalar_mul;
-use bandersnatch::EdwardsAffine;
-use bandersnatch::EdwardsProjective;
-use bandersnatch::Fr;
-#[derive(Debug, Clone)]
-pub struct CRS {
-    pub n: usize,
-    pub G: Vec<EdwardsProjective>,
-    pub Q: EdwardsProjective,
-}
+use std::collections::HashMap;
 
-impl CRS {
-    pub fn new(n: usize, seed: &'static [u8]) -> CRS {
-        let G: Vec<_> = generate_random_elements(n, seed)
-            .into_iter()
-            .map(|affine_point| affine_point.into_projective())
-            .collect();
-        let Q = EdwardsProjective::prime_subgroup_generator();
-        CRS { n, G, Q }
-    }
-
-    pub fn commit_lagrange_poly(&self, polynomial: &LagrangeBasis) -> EdwardsProjective {
-        slow_vartime_multiscalar_mul(polynomial.values().iter(), self.G.iter())
-    }
-}
-
-impl std::ops::Index<usize> for CRS {
-    type Output = EdwardsProjective;
-
-    fn index(&self, index: usize) -> &Self::Output {
-        &self.G[index]
-    }
-}
-
-fn generate_random_elements(num_required_points: usize, seed: &'static [u8]) -> Vec<EdwardsAffine> {
-    use bandersnatch::Fq;
-    use sha2::{Digest, Sha256};
-
-    let mut hasher = Sha256::new();
-
-    hasher.update(seed);
-    let bytes = hasher.finalize().to_vec();
-
-    let u = bandersnatch::Fq::from_be_bytes_mod_order(&bytes);
-    let choose_largest = false;
-
-    (0..)
-        .into_iter()
-        .map(|i| Fq::from(i as u128) + u)
-        .map(|x| EdwardsAffine::get_point_from_x(x, choose_largest))
-        .filter_map(|point| point)
-        .filter(|point| point.is_in_correct_subgroup_assuming_on_curve())
-        .take(num_required_points)
-        .collect()
-}
-
+use banderwagon::{multi_scalar_mul, Element, Fr};
 pub struct MultiPoint;
 
 #[derive(Clone, Debug)]
 pub struct ProverQuery {
-    pub commitment: EdwardsProjective,
+    pub commitment: Element,
     pub poly: LagrangeBasis, // TODO: Make this a reference so that upstream libraries do not need to clone
     // Given a function f, we use z_i to denote the input point and y_i to denote the output, ie f(z_i) = y_i
     pub point: usize,
@@ -93,7 +38,7 @@ impl From<ProverQuery> for VerifierQuery {
     }
 }
 pub struct VerifierQuery {
-    pub commitment: EdwardsProjective,
+    pub commitment: Element,
     pub point: Fr,
     pub result: Fr,
 }
@@ -223,7 +168,7 @@ impl MultiPoint {
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct MultiPointProof {
     open_proof: IPAProof,
-    g_x_comm: EdwardsProjective,
+    g_x_comm: Element,
 }
 
 impl MultiPointProof {
@@ -234,9 +179,9 @@ impl MultiPointProof {
         let g_x_comm_bytes = &bytes[0..32];
         let ipa_bytes = &bytes[32..]; // TODO: we should return a Result here incase the user gives us bad bytes
 
-        let point: EdwardsAffine = CanonicalDeserialize::deserialize(g_x_comm_bytes)
+        let point: Element = CanonicalDeserialize::deserialize(g_x_comm_bytes)
             .map_err(|_| IOError::from(IOErrorKind::InvalidData))?;
-        let g_x_comm = point.into_projective();
+        let g_x_comm = point;
 
         let open_proof = IPAProof::from_bytes(ipa_bytes, poly_degree)?;
         Ok(MultiPointProof {
@@ -325,7 +270,7 @@ pub(crate) fn open_point_outside_of_domain(
     precomp: &PrecomputedWeights,
     transcript: &mut Transcript,
     polynomial: LagrangeBasis,
-    commitment: EdwardsProjective,
+    commitment: Element,
     z_i: Fr,
 ) -> IPAProof {
     let a = polynomial.values().to_vec();
@@ -552,52 +497,4 @@ fn multiproof_consistency() {
     let got = hex::encode(bytes);
     let expected = "1e575ed50234769345382d64f828d8dd65052cc623c4bfe6dd1ca0a8eb6940de717d20b92f592aea4e1a649644ee92d83813e8e296c71e2d32b40532f455d8b9b56baadafbe84808d784aa920836b73af49d758bd8bb1a2690df8b2450d2112e3a48a06378bc60dffa9cd9f80c9c4da0385a388fc8edeca1a740d76b3ab1d8d3ccb0387a0c2005432d6a52e98ca46c0649a69b6b02b9832b1e108199e6977c403624cfff05715445e37586444a27d8c97f18b3bbf417b442e8c8ab16dfe3b0e96ba20178280e6192f8e4e861a21215f394c1ff3057cd5492d1a5154ed8330f3f93f7f02079042c27d51c6299904eadaf6e1e290cc94920d143112ddb34cf2488131bc321ff0349150aad44563ac765905b15b30ac71ebb01c78d7e26e4f920219d040fb50fab3a233ea349fe5e09b1c7e56b311dc8e4505c04c60e27c86d8cbb72a0fe057815972f4bf2e126684a79ba5a3932a9713e059cd51d1a8f0599efa54172d4dfae7016ce2b7b2b325ba847782a2741ba560c158e38d10362a61a11538dd3c5e6742bb96901f53291649fbad13518c79c40af9733f5b54743f7fba3cda82d56894d0265f0befbc2e8a45612411e9bde4123263b1cde7c76ede1b21d97694382416b8c8f502f2c9af06bf250095122fbbfada1b683f588aa01a654a2ddd736135729835790845b3c403cc793bbfc808dba33b7af33bb43d49e06595a095ac84290e268e41d72ef9b93d4bafd0bf537179621a1c4936a5b7f713e9dd5f0cec2779933f46e0d8f48f15a81565de89df43e727e834de5386e446ca2696a13";
     assert_eq!(got, expected)
-}
-
-#[test]
-fn crs_consistency() {
-    // See: https://hackmd.io/1RcGSMQgT4uREaq1CCx_cg#Methodology
-    use ark_serialize::CanonicalSerialize;
-    use bandersnatch::Fq;
-    use sha2::{Digest, Sha256};
-
-    let points = generate_random_elements(256, b"eth_verkle_oct_2021");
-    for point in &points {
-        let on_curve = point.is_on_curve();
-        let in_correct_subgroup = point.is_in_correct_subgroup_assuming_on_curve();
-        if !on_curve {
-            panic!("generated a point which is not on the curve")
-        }
-        if !in_correct_subgroup {
-            panic!("generated a point which is not in the prime subgroup")
-        }
-    }
-
-    let mut bytes = [0u8; 32];
-    points[0].serialize(&mut bytes[..]).unwrap();
-    assert_eq!(
-        hex::encode(&bytes),
-        "22ac968a98ab6c50379fc8b039abc8fd9aca259f4746a05bfbdf12c86463c208",
-        "the first point is incorrect"
-    );
-    let mut bytes = [0u8; 32];
-    points[255].serialize(&mut bytes[..]).unwrap();
-    assert_eq!(
-        hex::encode(&bytes),
-        "c8b4968a98ab6c50379fc8b039abc8fd9aca259f4746a05bfbdf12c86463c208",
-        "the 256th (last) point is incorrect"
-    );
-
-    let mut hasher = Sha256::new();
-    for point in &points {
-        let mut bytes = [0u8; 32];
-        point.serialize(&mut bytes[..]).unwrap();
-        hasher.update(&bytes);
-    }
-    let bytes = hasher.finalize().to_vec();
-    assert_eq!(
-        hex::encode(&bytes),
-        "c390cbb4bc42019685d5a01b2fb8a536d4332ea4e128934d0ae7644163089e76",
-        "unexpected point encountered"
-    );
 }
