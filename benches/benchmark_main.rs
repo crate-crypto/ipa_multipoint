5b26b7a44efdd6829f5d0994686555363329df21
Ignacio Hagopian <jsign.uy@gmail.com>
2023-05-24 12:10:54 -0300

    bench/ipa: create benchmark for proving
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- benches/benchmark_main.rs
+++ benches/benchmark_main.rs
@@ -2,6 +2,7 @@ use criterion::criterion_main;
 
 mod benchmarks;
 criterion_main! {
+    benchmarks::ipa_prove::benches,
     benchmarks::ipa_verify::benches,
     benchmarks::multipoint_verify::benches,
     benchmarks::multipoint_prove::benches,
